695a99a75e9baadb78eae0f84c292738da9a7112
Nick Artman <nicolasartman@gmail.com>
2024-09-07 00:11:39 -0700

    Upgrade build dependencies and fix build issues

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,25 +1,20 @@
-// Do not remove this import. If you do Vite will think your styles are dead
-// code and not include them in the build output.
-import "../styles/style.scss";
-import DogBrowser from "./apps/dogBrowser";
-import { moduleId } from "./constants";
-import { MyModule } from "./types";
+import DogBrowser from "./apps/dogBrowser"
+import { moduleId } from "./constants"
+import { MyModule } from "./types"
 
-let module: MyModule;
+let module: MyModule
 
 Hooks.once("init", () => {
-  console.log(`Initializing ${moduleId}`);
+  console.log(`Initializing ${moduleId}`)
 
-  module = (game as Game).modules.get(moduleId) as MyModule;
-  module.dogBrowser = new DogBrowser();
-});
+  module = (game as Game).modules.get(moduleId) as MyModule
+  module.dogBrowser = new DogBrowser()
+})
 
 Hooks.on("renderActorDirectory", (_: Application, html: JQuery) => {
-  const button = $(
-    `<button class="cc-sidebar-button" type="button">🐶</button>`
-  );
+  const button = $(`<button class="cc-sidebar-button" type="button">🐶</button>`)
   button.on("click", () => {
-    module.dogBrowser.render(true);
-  });
-  html.find(".directory-header .action-buttons").append(button);
-});
+    module.dogBrowser.render(true)
+  })
+  html.find(".directory-header .action-buttons").append(button)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import * as fsPromises from "fs/promises";
-import copy from "rollup-plugin-copy";
-import scss from "rollup-plugin-scss";
-import { defineConfig, Plugin } from "vite";
+import * as fsPromises from "fs/promises"
+import copy from "rollup-plugin-copy"
+import { defineConfig, Plugin } from "vite"
 
-const moduleVersion = process.env.MODULE_VERSION;
-const githubProject = process.env.GH_PROJECT;
-const githubTag = process.env.GH_TAG;
+const moduleVersion = process.env.MODULE_VERSION
+const githubProject = process.env.GH_PROJECT
+const githubTag = process.env.GH_TAG
 
-console.log(process.env.VSCODE_INJECTION);
+console.log(process.env.VSCODE_INJECTION)
 
 export default defineConfig({
   build: {
@@ -15,59 +14,44 @@ export default defineConfig({
     rollupOptions: {
       input: "src/ts/module.ts",
       output: {
-        dir: undefined,
-        file: "dist/scripts/module.js",
+        dir: "dist/scripts",
+        entryFileNames: "module.js",
         format: "es",
       },
     },
   },
   plugins: [
     updateModuleManifestPlugin(),
-    scss({
-      output: "dist/style.css",
-      sourceMap: true,
-      watch: ["src/styles/*.scss"],
-    }),
     copy({
       targets: [
         { src: "src/languages", dest: "dist" },
         { src: "src/templates", dest: "dist" },
+        { src: "src/styles/style.css", dest: "dist", rename: "style.css" },
       ],
       hook: "writeBundle",
     }),
   ],
-});
+})
 
 function updateModuleManifestPlugin(): Plugin {
   return {
     name: "update-module-manifest",
     async writeBundle(): Promise<void> {
       const packageContents = JSON.parse(
-        await fsPromises.readFile("./package.json", "utf-8")
-      ) as Record<string, unknown>;
-      const version = moduleVersion || (packageContents.version as string);
-      const manifestContents: string = await fsPromises.readFile(
-        "src/module.json",
-        "utf-8"
-      );
-      const manifestJson = JSON.parse(manifestContents) as Record<
-        string,
-        unknown
-      >;
-      manifestJson["version"] = version;
+        await fsPromises.readFile("./package.json", "utf-8"),
+      ) as Record<string, unknown>
+      const version = moduleVersion || (packageContents.version as string)
+      const manifestContents: string = await fsPromises.readFile("src/module.json", "utf-8")
+      const manifestJson = JSON.parse(manifestContents) as Record<string, unknown>
+      manifestJson["version"] = version
       if (githubProject) {
-        const baseUrl = `https://github.com/${githubProject}/releases`;
-        manifestJson["manifest"] = `${baseUrl}/latest/download/module.json`;
+        const baseUrl = `https://github.com/${githubProject}/releases`
+        manifestJson["manifest"] = `${baseUrl}/latest/download/module.json`
         if (githubTag) {
-          manifestJson[
-            "download"
-          ] = `${baseUrl}/download/${githubTag}/module.zip`;
+          manifestJson["download"] = `${baseUrl}/download/${githubTag}/module.zip`
         }
       }
-      await fsPromises.writeFile(
-        "dist/module.json",
-        JSON.stringify(manifestJson, null, 4)
-      );
+      await fsPromises.writeFile("dist/module.json", JSON.stringify(manifestJson, null, 4))
     },
-  };
+  }
 }
